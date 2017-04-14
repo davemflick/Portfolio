112e3c9ea7f63360ba0e6a99dd951e846c5fb5c7
davemflick <davemflick@yahoo.com>
2017-04-14 08:35:10 -0400

    added data-viz cert

--- app/components/Certificates.js
+++ app/components/Certificates.js
@@ -7,7 +7,8 @@ export default class Certificates extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			styleFcc: {display: "none"},
+			styleFccFE: {display: "none"},
+			styleFccDV: {display: "none"},
 			styleStan: {display: "none"},
 			style: {display: "none"},
 		}
@@ -15,8 +16,12 @@ export default class Certificates extends React.Component {
 
  
 	handleClick (e) {
-		if(e.target.id == "fcc") {
-			this.state.styleFcc = {display: "inline-block"};
+		if(e.target.id == "fccFE") {
+			this.state.styleFccFE = {display: "inline-block"};
+			this.state.style = {display: "inline-block"};
+		}
+		else if(e.target.id == "fccDV") {
+			this.state.styleFccDV = {display: "inline-block"};
 			this.state.style = {display: "inline-block"};
 		}
 		else if(e.target.id == "stanford") {
@@ -24,7 +29,8 @@ export default class Certificates extends React.Component {
 			this.state.style = {display: "inline-block"};
 		}
 		if(e.target.className == "exit" || e.target.className == "certs") {
-			this.state.styleFcc = {display: "none"};
+			this.state.styleFccFE = {display: "none"};
+			this.state.styleFccDV = {display: "none"};
 			this.state.styleStan = {display: "none"};
 			this.state.style = {display: "none"};
 		}
@@ -37,13 +43,19 @@ export default class Certificates extends React.Component {
 			<div className="certContainer" id="cert">
 				<h3> Certificates </h3>
 				<div className="certName">
-					<h4 id="fcc" onClick={this.handleClick.bind(this)}> freeCodeCamp Front-End </h4>
+					<h4 id="fccFE" onClick={this.handleClick.bind(this)}> FCC Front End </h4>
+					<h4 id="fccDV" onClick={this.handleClick.bind(this)}> FCC Data Viz </h4>
 					<h4 id="stanford" onClick={this.handleClick.bind(this)}> Stanford cs101 </h4>
 				</div>
 				<div className="certs" style={this.state.style} onClick={this.handleClick.bind(this)}>
 
-					<div className="popUpFcc" style={this.state.styleFcc}>
+					<div className="popUpFccFE" style={this.state.styleFccFE}>
 						<img id="fccFE" src="./app/styles/images/fccFrontEnd.png" alt="freeCodeCamp Front-End" />
+						<button className="exit" onClick={this.handleClick.bind(this)}> X </button>
+					</div>
+
+					<div className="popUpFccDV" style={this.state.styleFccDV}>
+						<img id="fccDV" src="./app/styles/images/dataViz.png" alt="freeCodeCamp Data Viz" />
 						<button className="exit" onClick={this.handleClick.bind(this)}> X </button>
 					</div>
 
