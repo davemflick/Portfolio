e82a82910263b9bcc41aa0e2b12e73bf2d876825
davemflick <davemflick@yahoo.com>
2017-04-22 10:32:50 -0400

    added timestamp project and skill icons

--- app/components/Technologies.js
+++ app/components/Technologies.js
@@ -13,6 +13,8 @@ export default class Technologies extends React.Component {
 			    <i className="devicon-html5-plain-wordmark colored"></i>
 			    <i className="devicon-css3-plain-wordmark colored"></i>
 			    <i className="devicon-javascript-plain colored"></i>
+			    <i className="devicon-jquery-plain-wordmark" id="jQuery"></i>
+			    <i className="devicon-nodejs-plain-wordmark colored" id="nodejs"></i>
 			    <i className="devicon-bootstrap-plain-wordmark" id="bootstrap"></i>
 			    <i className="devicon-sass-original colored"></i>
 			    <i className="devicon-react-original-wordmark colored"></i>
@@ -20,6 +22,7 @@ export default class Technologies extends React.Component {
 			    <i className="devicon-babel-plain colored"></i>
 			    <i className="devicon-git-plain colored"></i>
 			    <i className="devicon-github-plain colored"></i>
+			    <i className="devicon-chrome-plain-wordmark colored"></i>
 			  </div>
 			</div>
 		)
